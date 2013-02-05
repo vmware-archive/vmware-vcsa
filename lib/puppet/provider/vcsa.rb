e67ee3da20371bd24d71af5513e6e4aeb2567791
Nan Liu <nan.liu@gmail.com>
2013-02-05 13:09:20 -0800

    Fix loading of transport.

--- lib/puppet/provider/vcsa.rb
+++ lib/puppet/provider/vcsa.rb
@@ -1,6 +1,7 @@
 require 'pathname' # WORK_AROUND #14073 and #7788
 module_lib = Pathname.new(__FILE__).parent.parent.parent
-require File.join module_lib, 'puppet_x/puppetlabs/transport'
+vmware_module = Puppet::Module.find('vmware', Puppet[:environment].to_s)
+require File.join vmware_module.path, 'lib/puppet_x/puppetlabs/transport'
 require File.join module_lib, 'puppet_x/puppetlabs/transport/ssh'
 
 class Puppet::Provider::Vcsa <  Puppet::Provider
