33c9ad9bcd1c3694b43a66a9057f9c16c1c4901d
Kevin Murphy <maniacmurphy@github.com>
2015-03-27 03:50:45 +0000

    SSO Updates and Enhanced Error checking to providers -Changed some vcsa_sso parameters to properties to allow for them to be changed after the initial configurations -Updated vcsa_sso provider to have methods for modifying parameters -Added a catch to all providers to catch when vpxd_servicecfg exits with failures

--- lib/puppet/provider/vcsa.rb
+++ lib/puppet/provider/vcsa.rb
@@ -13,4 +13,9 @@ class Puppet::Provider::Vcsa <  Puppet::Provider
   confine :feature => :ssh
 
   include PuppetX::Puppetlabs::Transport
+
+  def servicecfg_catch(result)
+    error = Hash[*result.split("\n").map{|x| x.split("=",2) if x =~ /^(?!Key not found)/ && x =~ /^(?!Stopping)/ }.compact.flatten]
+    raise Puppet::Error, "vpxd_servicecfg failure: Returned #{result.sub(/\n/, ', ')}" if error['VC_CFG_RESULT'] != '0'
+  end
 end
